6e37fdf509c12dca2ea5d0c23ba0b5c4201aa6b9
Tung Dao <me@tungdao.com>
2019-09-26 15:54:32 +0700

    add: simple counter

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
+import logging
 import os
+from types import SimpleNamespace
 
-from prometheus_client import make_wsgi_app
+from prometheus_client import Counter, Summary, make_wsgi_app
 from tornado import ioloop, options, web, wsgi
+from tornado.escape import json_decode, json_encode
 
 options.define('debug', default=os.getenv('ENV') == 'development')
 options.define('port', default=8888, type=int)
 
+metrics = SimpleNamespace(
+    request_time=Summary('request_processing_seconds', 'Time spent processing request'),
+    commit_count=Counter('git_commit_count', 'Commit count', ['repository'])
+)
 
-class GithubWebhookHandler(web.RequestHandler):
-    async def post(self):
-        pass
+
+class JsonMixin(web.RequestHandler):
+    def prepare(self):
+        super().prepare()
+        if self.request.headers.get('Content-Type') == 'application/json':
+            self.request.json = json_decode(self.request.body)
+
+    def respond(self, data):
+        self.set_header('Content-Type', 'application/json')
+        self.write(json_encode(data))
+
+    def get_argument(self, name, default=None, strip=True):
+        if hasattr(self.request, 'json'):
+            return self.request.json.get(name, default)
+        return super().get_argument(name, default, strip)
+
+
+class GithubWebhookHandler(JsonMixin, web.RequestHandler):
+    @metrics.request_time.time()
+    def post(self):
+        logging.debug(self.request.json)
+        event = self.request.headers['X-GitHub-Event']
+        if event == 'push':
+            repository = self.get_argument('repository')['full_name']
+            metrics.commit_count.labels(repository).inc(len(self.get_argument('commits')))
+        self.respond({'success': True})
 
 
 class Application(web.Application):
